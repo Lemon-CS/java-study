a30aea166efe5b27da55a03abae98a4958d82250
fangpeng <591930734@qq.com>
2022-02-08 14:15:26 +0800

    添加组合模式demo

---
+++ src/main/java/com/fangpeng/design/pattern/combination/MenuComponent.java
@@ -0,0 +1,38 @@
+package com.fangpeng.design.pattern.combination;
+
+/**
+ * 菜单组件 ： 属于抽象根节点
+ * @author 方鹏
+ * @date 2022年02月08日 2:02 下午
+ */
+public abstract class MenuComponent {
+
+    //菜单组件的名称
+    protected String name;
+    //菜单组件的层级
+    protected int level;
+
+    //添加子菜单
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    //移除子菜单
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    //获取指定的子菜单
+    public MenuComponent getChild(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    //获取菜单或者菜单项的名称
+    public String getName() {
+        return name;
+    }
+
+    //打印菜单名称的方法（包含子菜单和字菜单项）
+    public abstract void print();
+
+}
---
+++ src/main/java/com/fangpeng/design/pattern/combination/MenuItem.java
@@ -0,0 +1,24 @@
+package com.fangpeng.design.pattern.combination;
+
+/**
+ * 菜单项类 ： 属于叶子节点
+ * @author 方鹏
+ * @date 2022年02月08日 2:12 下午
+ */
+public class MenuItem extends MenuComponent {
+
+    public MenuItem(String name,int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    @Override
+    public void print() {
+        //打印菜单项的名称
+        for(int i = 0; i < level; i++) {
+            System.out.print("--");
+        }
+        System.out.println(name);
+    }
+
+}
