1aa6985aa05b790664e77d4f26bf5cd2021e42be
fangpeng <591930734@qq.com>
2022-02-14 23:11:53 +0800

    添加mybatis之源码阅读分析

---
+++ mybatis-3-master/src/test/java/com/lemon/mapper/IUserMapper.java
@@ -0,0 +1,14 @@
+package com.lemon.mapper;
+
+import com.lemon.pojo.User;
+
+import java.util.List;
+
+public interface IUserMapper {
+
+    //查询所有用户信息，同时查询出每个用户关联的订单信息
+    public List<User>  findAll();
+
+
+    public User findById(Integer id);
+}
