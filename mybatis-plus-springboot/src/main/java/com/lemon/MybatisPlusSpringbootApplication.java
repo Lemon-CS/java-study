aacae047012e5cd247f56295c5e71bf692f0f02b
fangpeng <591930734@qq.com>
2022-02-15 22:27:52 +0800

    添加springboot整合MyBatis-Plus

--- mybatis-plus-springboot/src/main/java/com/lemon/MybatisPlusSpringbootApplication.java
+++ mybatis-plus-springboot/src/main/java/com/lemon/MybatisPlusSpringbootApplication.java
@@ -1,8 +1,10 @@
 package com.lemon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.lemon.mapper")
 @SpringBootApplication
 public class MybatisPlusSpringbootApplication {
 
--- mybatis-plus-springboot/src/test/java/com/lemon/MybatisPlusSpringbootApplicationTests.java
+++ mybatis-plus-springboot/src/test/java/com/lemon/MybatisPlusSpringbootApplicationTests.java
@@ -1,13 +1,28 @@
 package com.lemon;
 
+import com.lemon.mapper.UserMapper;
+import com.lemon.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MybatisPlusSpringbootApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    void testSelect() {
+        List<User> userList = userMapper.selectList(null);
+        for (User user : userList) {
+            System.out.println(user);
+        }
     }
 
 }
